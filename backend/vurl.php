eb681020e14da2be82cdff7b25a235b02bb7a7b7
Lucas <83518257+luqaska@users.noreply.github.com>
2022-04-24 22:37:42 +0000

    Update vurl.php

--- backend/vurl.php
+++ backend/vurl.php
@@ -1,5 +1,6 @@
 <?php
-/* vurltool 1.0 */
+/* vurltool 1.0
+https://gist.github.com/luqaska/836caf2b121863df405f430d79d825b4 */
 $vrl = $_SERVER["REQUEST_URI"];
 $vrl = preg_replace("/#(.*)$/", "", $vrl);
 $vrl = preg_replace("/\?(.*)$/", "", $vrl);
